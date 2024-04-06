085b1db116249e29ac6c3dbf1ad4d436ae50e007
rohitGupta2022 <pta.rohit28@gmail.com>
2024-04-06 16:03:22 +0530

    commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyA2VDUwbDrtEHc5ChKtpxYRwF0I_m1VQvg",
+	authDomain: "crypto-hunter-c2386.firebaseapp.com",
+	projectId: "crypto-hunter-c2386",
+	storageBucket: "crypto-hunter-c2386.appspot.com",
+	messagingSenderId: "427625241159",
+	appId: "1:427625241159:web:1cb1aff0c5ddf9fc362f62",
+};
+export default firebaseConfig;
